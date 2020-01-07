c64a7923a5e62919e116d97a09f5cbe05482255d
Yahor Hayeuski <di23@users.noreply.github.com>
2020-01-08 00:48:16 +0300

    init commit

---
+++ compare_comps_aescript.jsx
@@ -0,0 +1,10 @@
+/*
+    compare_comps_aescript
+    Author: Yahor Hayeuski
+    version: 0.1.0
+
+    After Effects script. Compare two comps and return the difference in text format.
+*/
+( function (){
+
+})(); // end
